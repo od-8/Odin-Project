050d4551f96d6d1a04711f083d639bd932459c82
od-8 <od30338@gmail.com>
2024-11-20 22:52:25 +0000

    Add files via upload

---
+++ stock_picker.rb
@@ -0,0 +1,19 @@
+def pick_stocks (value)
+  
+  end
+end
+pick_stocks([7, 9, 7, 14, 21, 18, 3])
+
+
+
+
+
+#pick_stocks([4, 7, 6, 9, 11, 17, 8])
+=begin 
+array = [3, 7, 8, 4]
+index =  value.index(value.min)
+puts index 
+=end
+
+#def can_buy_sell 
+  #if value.index(value.max) < value.index(value.min) 
