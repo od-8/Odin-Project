e902d079053a171d2e81c0ae7f9c4d284bb98e69
od-8 <od30338@gmail.com>
2024-12-30 19:10:52 +0000

    tried and failed

--- naughts_and_crosses.rb/lib/mk2/naughts_crosses_mk2.rb
+++ naughts_and_crosses.rb/lib/mk2/naughts_crosses_mk2.rb
@@ -17,12 +17,26 @@ puts ""
 puts "Input length"
 length = gets.chomp.to_i
 
-
-#print_board
-
+module Victory
+  def horizontal_win(board, length)
+    winning_x = []
+    winning_o = []
+    length.each do
+      winning_x.push("X")
+      winning_o.push("O")
+    end
+    if board.include?(winning_o) == true || board.include?(winning_x) == true
+      puts "Somebody has won horizontally."
+    end
+  end
+end
 class Board
   def initialize(height, length)
     @squares = Array.new(1) { Array.new(height) {Array.new(length)} }
+  end
+  include Victory
+  
+  def print_board
     puts ""
     @squares.each do |array|
       array.each do |array|
@@ -33,6 +47,43 @@ class Board
   end
 end
 
-my_board = Board.new(height, length)
+class Places
+  def initialize(row, column, letter)
+    @row = row - 1
+    @column = column - 1
+    @player = letter
 
-#my_board.print_board
+    if my_board[@row][@column] != nil
+      puts "Square is full"
+      $num -= 1
+    else
+      my_board[@row][@column] = letter
+    end
+
+    Squares.each do |array|
+      puts "#{array}\n"
+    end
+
+    puts ""
+  end
+end
+
+while true 
+  puts "Input row (1 - 3)"
+  row = gets.chomp.to_i
+  puts ""
+  puts "Input column (1 - 3)"
+  column = gets.chomp.to_i
+  puts ""
+  puts "Input letter (X or O)"
+  letter = gets.chomp
+  puts ""
+
+  my_board = Board.new(height, length)
+
+  my_board.horizontal_win(my_board, length)
+
+  my_board.print_board
+
+  Places.new(row, column, letter)
+end
