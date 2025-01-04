826a712664c35173210b7e2bc4aeef13584b0f9e
od-8 <od30338@gmail.com>
2025-01-04 22:04:49 +0000

    Finished mk1, horrible code, must fix mk2

--- mastermind/lib/mk1/mastermind_mk1.rb
+++ mastermind/lib/mk1/mastermind_mk1.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: false
 
+require 'colorize'
+
+# Global vars
+$i = -1
+$rp = -1
+$game_over = -1
+$num = 0
+
 # The board class
 class Board
   def initialize(first, second, third, fourth)
@@ -12,25 +20,12 @@ class Board
   end
 
   def print_board
-    puts '+---+---+---+---+'
-    puts "| #{@board[0][0]} | #{@board[0][1]} | #{@board[0][2]} | #{@board[0][3]} |"
-    puts '+---+---+---+---+'
-    puts "| #{@board[1][0]} | #{@board[1][1]} | #{@board[1][2]} | #{@board[1][3]} |"
-    puts '+---+---+---+---+'
-    puts "| #{@board[2][0]} | #{@board[2][1]} | #{@board[2][2]} | #{@board[2][3]} |"
-    puts '+---+---+---+---+'
-    puts "| #{@board[3][0]} | #{@board[3][1]} | #{@board[3][2]} | #{@board[3][3]} |"
-    puts '+---+---+---+---+'
-    puts "| #{@board[4][0]} | #{@board[4][1]} | #{@board[4][2]} | #{@board[4][3]} |"
-    puts '+---+---+---+---+'
     puts ''
-    puts "Winning combo = #{@winning_combo}"
+    @board.each do |array|
+      puts "#{array} \n"
+    end
+    puts ''
   end
-
-  $i = -1
-  $n = -1
-  $x = 0
-  $z = 0
 
   def add_colors(one, two, three, four)
     $i += 1
@@ -40,75 +35,166 @@ class Board
     @board[$i][3] = four
   end
 
-  def right_place?
-    $n += 1
-    true if
-    @board[$n].any? { |i| i == @winning_combo[0] } ||
-    @board[$n].any? { |i| i == @winning_combo[1] } ||
-    @board[$n].any? { |i| i == @winning_combo[2] } ||
-    @board[$n].any? { |i| i == @winning_combo[3] }
+  # This checks if the input is valid
+  def real_color?(colors, input)
+    true if colors.include?(input)
   end
 
-  def win?
-    true if @board[0] == @winning_combo
+  def correct
+    $rp += 1
+    right_position = @board[$rp].each_with_index.select { |item, index| @winning_combo[index] == item }.map(&:first)
+    wrong_position = @board[$rp].select { |i| @winning_combo.include?(i) && !right_position.include?(i) }
+    right_position_size = right_position.length
+    wrong_position_size = wrong_position.length
+
+    if right_position_size == 4
+      puts 'Congratulations Player 1 you have won.'
+      puts ''
+      $game_over = 1
+    elsif right_position_size.between?(2, 3)
+      puts "#{right_position.join(' and ').capitalize} are correct and in the right positions."
+      puts ''
+    elsif right_position_size == 1
+      puts "#{right_position.join('').capitalize} is correct and in the correct position."
+      puts ''
+    end
+
+    if wrong_position_size == 4
+      puts 'They are all correct but not in the right positions.'
+      puts ''
+    elsif wrong_position_size.between?(2, 4)
+      puts "#{wrong_position.join(' and ').capitalize} are correct but in the wrong position."
+      puts ''
+    elsif wrong_position_size == 1
+      puts "#{wrong_position.join('').capitalize} is correct but in the wrong position."
+      puts ''
+    end
   end
 
-  def full?
-    true if $z == 5 
+  def full
+    unless !@board[0].include?('-') && !@board[1].include?('-') && !@board[2].include?('-') && !@board[3].include?('-') && !@board[4].include?('-')
+      return
+    end
+
+    puts 'Player 2, you could not guess the correct combination of colors so Player 1 you win!'
+    $game_over = 1
   end
 end
 
-# puts 'Lets play mastermind'
-# puts ''
-# puts 'Player 1 will be guessing.'
-# puts ''
-# puts 'player 2 will input the 4 correct colors.'
-# puts ''
-puts 'Input the first color'
-color_1 = gets.chomp.downcase
+puts 'Lets play mastermind'
 puts ''
-puts 'Input the second color'
-color_2 = gets.chomp.downcase
+puts 'Player 1 will be guessing.'
 puts ''
-puts 'Input the third color'
-color_3 = gets.chomp.downcase
+puts 'player 2 will try to input the 4 correct colors.'
 puts ''
-puts 'Input the fourth color'
-color_4 = gets.chomp.downcase
+
+colors = %w[red green blue yellow magenta cayan white]
+puts 'These are all the colors you chan choose from:'
+
+colors.each do |color|
+  puts " - #{color.capitalize}\n"
+end
+
 puts ''
-my_board = Board.new(color_1, color_2, color_3, color_4)
+puts 'Player 1, input the first color'
+color_one = gets.chomp.downcase
+puts ''
+
+until colors.include?(color_one)
+  puts 'That is not a valid color, Choose one from the list above.'
+  puts 'Player 1 input your first color.'
+  color_one = gets.chomp.downcase
+  puts ''
+end
+
+puts 'Player 1, input the second color'
+color_two = gets.chomp.downcase
+puts ''
+
+until colors.include?(color_two)
+  puts 'That is not a valid color, Choose one from the list above.'
+  puts 'Player 1 input your second color.'
+  color_two = gets.chomp.downcase
+  puts ''
+end
+
+puts 'Player 1, input the third color'
+color_three = gets.chomp.downcase
+puts ''
+
+until colors.include?(color_three)
+  puts 'That is not a valid color, Choose one from the list above.'
+  puts 'Player 1 input your third color.'
+  color_three = gets.chomp.downcase
+  puts ''
+end
+
+puts 'Player 1, input the fourth color'
+color_four = gets.chomp.downcase
+puts ''
+
+until colors.include?(color_four)
+  puts 'That is not a valid color, Choose one from the list above.'
+  puts 'Player 1 input your fourth color.'
+  color_four = gets.chomp.downcase
+  puts ''
+end
+
+my_board = Board.new(color_one, color_two, color_three, color_four)
 my_board.print_board
 
-until $x == 1 do
-  puts 'Input your first guess'
-  guess_1 = gets.chomp.downcase
+until $game_over == 1
+  $num += 1
+  puts "Num = #{$num}"
+  puts 'Player 2 Input your first guess'
+  guess_one = gets.chomp.downcase
   puts ''
-  puts 'Input your second guess'
-  guess_2 = gets.chomp.downcase
+
+  until my_board.real_color?(colors, guess_one)
+    puts 'That is not a valid color, Choose one from the list above.'
+    puts 'Player 2 input your first guess'
+    guess_one = gets.chomp.downcase
+    puts ''
+  end
+
+  puts 'Player 2 input your second guess'
+  guess_two = gets.chomp.downcase
   puts ''
-  puts 'Input your thrid guess'
-  guess_3 = gets.chomp.downcase
+
+  until my_board.real_color?(colors, guess_two)
+    puts 'That is not a valid color, Choose one from the list above.'
+    puts 'Player 2 input your second guess'
+    guess_two = gets.chomp.downcase
+    puts ''
+  end
+
+  puts 'Player 2 input your third guess'
+  guess_three = gets.chomp.downcase
   puts ''
-  puts 'Input your fourth guess'
-  guess_4 = gets.chomp.downcase
+
+  until my_board.real_color?(colors, guess_three)
+    puts 'That is not a valid color, Choose one from the list above.'
+    puts 'Player 2 input your third guess'
+    guess_three = gets.chomp.downcase
+    puts ''
+  end
+
+  puts 'Player 2 input your fourth guess'
+  guess_four = gets.chomp.downcase
   puts ''
-  # puts 'Processing...'
-  # puts ''
-  # sleep 1
-  my_board.add_colors(guess_1, guess_2, guess_3, guess_4)
+
+  until my_board.real_color?(colors, guess_four)
+    puts 'That is not a valid color, Choose one from the list above.'
+    puts 'Player 2 input your fourth guess'
+    guess_four = gets.chomp.downcase
+    puts ''
+  end
+
+  my_board.add_colors(guess_one, guess_two, guess_three, guess_four)
+
   my_board.print_board
 
-  puts '  A letter is correct' if my_board.right_place?
-  if my_board.win?
-    puts "  Sorry player 1 you're you colors were to easy to guess, congrats player 2!"
-    $x = 1
-  end
+  my_board.correct
 
-  if my_board.full?
-    $x = 1
-    puts '  Sorry player 2 you ran out of guesses, conragts player 1!'
-  end
-  $z += 1
+  my_board.full
 end
-
-my_board.print_board
