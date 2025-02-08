4be8d215cde127429a195c3790692064f0ab6ef4
od-8 <od30338@gmail.com>
2025-02-08 17:17:47 +0000

    Finished #insert and made #find

--- computer_science/lib/binary_search_trees.rb
+++ computer_science/lib/binary_search_trees.rb
@@ -14,6 +14,7 @@ class Tree
     @root = build_tree(array)
   end
 
+  # Creates the tree
   def build_tree(array)
     return nil if array.empty?
 
@@ -28,29 +29,38 @@ class Tree
     return root
   end
 
+  # Makes the tree easy to visualize (provided from lesson)
   def pretty_print(node = @root, prefix = '', is_left = true)
     pretty_print(node.right_child, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right_child
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
     pretty_print(node.left_child, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left_child
   end
 
+  # Creates a new node at end of branch
   def insert(value, root = @root)
-
-    if root&.data.nil?
-      
+    if root.left_child.nil? && root.right_child.nil? 
+      root.left_child = Node.new(value) if value < root.data
+      root.right_child = Node.new(value) if value > root.data
     elsif value < root.data
       insert(value, root.left_child)
     elsif value > root.data
       insert(value, root.right_child)
     end
+  end
 
-    p root
+  # Find the node of the provided value, returns nil if no matches
+  def find(value, root = @root)
+    if root&.data.nil?
+      puts "There are no nodes that have the value of: #{value}."
+    elsif root.data == value
+      puts "Node: #{root}, Value: #{value}."
+    elsif value < root.data
+      find(value, root.left_child)
+    elsif value > root.data
+      find(value, root.right_child)
+    end
   end
 end
 
 tree = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
 tree.pretty_print
-tree.insert(12)
-puts ''
-tree.pretty_print
-
